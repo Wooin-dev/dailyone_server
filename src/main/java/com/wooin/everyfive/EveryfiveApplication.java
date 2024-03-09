62b3521b0dd3f972074cf1d0f55325f7ea1b1941
hyeonwookim <wooin.dev@gmail.com>
2024-03-09 12:33:45 +0900

    git ignore push update

---
+++ src/main/java/com/wooin/everyfive/EveryfiveApplication.java
@@ -0,0 +1,13 @@
+package com.wooin.everyfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryfiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryfiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wooin/everyfive/EveryfiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooin.everyfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryfiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8176bfa89919629aa22a1944a3f156f52283974b
hyeonwookim <wooin.dev@gmail.com>
2024-03-29 00:44:46 +0900

    0328 TIL
    엔티티 equals 커스텀하기
    영속화됐다는 가정하에 PK인 id만 검증

--- src/main/java/com/wooin/dailyone/domain/DefaultEntity.java
+++ src/main/java/com/wooin/dailyone/domain/DefaultEntity.java
@@ -2,7 +2,9 @@ package com.wooin.dailyone.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,15 +17,21 @@ public abstract class DefaultEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long id;
 
     @CreatedDate
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
+    @CreatedBy
+    private String createdBy;
+
     @LastModifiedDate
     @Column
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    private String modifiedBy;
 }
--- src/main/java/com/wooin/dailyone/domain/Goal.java
+++ src/main/java/com/wooin/dailyone/domain/Goal.java
@@ -4,9 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+import java.util.Objects;
+
 @Entity
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table(indexes = {
@@ -18,14 +19,35 @@ import lombok.*;
 public class Goal extends DefaultEntity{
 
     @NotBlank
-    @Setter private String originalGoal;
+    @Setter @Column(nullable = false)   private String originalGoal;
+    @Setter                             private String simpleGoal;
+    @Setter @Column(nullable = false)   private String motivationComment;
+    @Setter @Column(nullable = false)   private String congratsComment;
 
-    @Setter private String simpleGoal;
-
-    @Setter private String motivationComment;
-
-    @Setter private String congratsComment;
-
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY)
     private User user;
+
+    private Goal(String originalGoal, String simpleGoal, String motivationComment, String congratsComment) {
+        super();
+        this.originalGoal = originalGoal;
+        this.simpleGoal = simpleGoal;
+        this.motivationComment = motivationComment;
+        this.congratsComment = congratsComment;
+    }
+
+    public static Goal of(String originalGoal, String simpleGoal, String motivationComment, String congratsComment) {
+        return new Goal(originalGoal, simpleGoal, motivationComment, congratsComment);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Goal goal)) return false;
+        return this.getId() != null && this.getId().equals(((Goal) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
 }

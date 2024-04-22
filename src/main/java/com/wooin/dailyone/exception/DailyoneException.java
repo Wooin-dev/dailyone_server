d495cec637bf51cf30478df5198512e594397fb7
Hyeon woo Kim <wooin.dev@gmail.com>
2024-04-22 14:34:31 +0900

    development : 예외관련 - 예외처리 Runtime예외 전역 추가 - 에러코드 일부 추가 - DailyoneException 생성자 추가(오버로딩)

--- src/main/java/com/wooin/dailyone/exception/DailyoneException.java
+++ src/main/java/com/wooin/dailyone/exception/DailyoneException.java
@@ -10,6 +10,11 @@ public class DailyoneException extends RuntimeException{
     private ErrorCode errorCode;
     private String message;
 
+    public DailyoneException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
     @Override
     public String getMessage() {
         if (message == null) {
--- src/main/java/com/wooin/dailyone/exception/ErrorCode.java
+++ src/main/java/com/wooin/dailyone/exception/ErrorCode.java
@@ -9,8 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email is duplicated"),
-    NOT_JOINED_EMAIL(HttpStatus.NOT_FOUND, "Email is not joined"),
+    NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, "Email is not joined"),
     INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is incorrect"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"),
     ;
 
     private HttpStatus status;

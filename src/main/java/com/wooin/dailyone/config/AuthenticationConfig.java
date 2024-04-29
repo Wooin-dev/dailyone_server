29acb6141431e2b71eeb7c71f792fcd0677c08d2
hyeonwookim <wooin.dev@gmail.com>
2024-04-29 11:51:41 +0900

    init : 정적자원에 대한 검증 ignoring  - WebSecurityCustomizer 빈추가 형태로 구현

--- src/main/java/com/wooin/dailyone/config/AuthenticationConfig.java
+++ src/main/java/com/wooin/dailyone/config/AuthenticationConfig.java
@@ -5,11 +5,13 @@ import com.wooin.dailyone.exception.CustomAuthenticationEntryPoint;
 import com.wooin.dailyone.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -25,6 +27,11 @@ public class AuthenticationConfig {
     private String key;
     private final UserService userService;
     private final CustomAuthenticationEntryPoint authenticationEntryPointHandler;
+
+    @Bean //정적파일에 대한 인증을 무시하기 위한 빈 등록.
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
